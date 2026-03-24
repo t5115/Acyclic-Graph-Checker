020fd3946b48060e8d68090ce62bb919aba853a7
Tahmid Ahmed <tahmid.ahmed5115@gmail.com>
2026-03-24 18:47:40 +0000

    Initial Commit

---
+++ graphSinks.cpp
@@ -0,0 +1,20 @@
+/* 
+5SENG003W Algorithms – Coursework (2025/26)
+Checking acyclicity of directed graphs
+*/
+
+#include <iostream>
+using namespace std;
+
+// Task 2 - Data structure for representing direct graphs ()
+
+// Task 3 - Parser that takes an input to generate a graph
+
+// Task 4 - Algorithm to check if a graph is acyclic, with checks to show how answer obtained.
+
+// Task 5 - Prints first cycle find in a graph if not acyclic
+
+// Program Start
+int main(){
+
+}
