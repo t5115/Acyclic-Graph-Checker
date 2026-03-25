37b5a3b77931554da25b52828de4903e3e8a267c
Tahmid Ahmed <tahmid.ahmed5115@gmail.com>
2026-03-25 12:07:11 +0000

    feat: added input parser to create adjacency matrix

--- graphSinks.cpp
+++ graphSinks.cpp
@@ -4,10 +4,14 @@ Checking acyclicity of directed graphs
 */
 
 #include <iostream>
-#include <vector>
+#include <vector> 
+#include <fstream> 
+#include <set>
+#include <map>
+
 using namespace std;
 
-// Task 2 - Data structure for representing direct graphs ()
+// Task 2 - Data structure for representing direct graphs (adjacency matrix)
 class Graph {
     private:
     int size; // number of nodes
@@ -19,6 +23,10 @@ class Graph {
         matrix.resize(n, vector<int>(n, 0));
     }
     
+    void addEdge(int from, int to){
+        matrix[from][to] = 1;
+    }
+
     void print(){
         for(int i=0;i<size;i++){
             for(int j=0;j<size;j++){
@@ -31,6 +39,48 @@ class Graph {
 };
 
 // Task 3 - Parser that takes an input to generate a graph
+Graph InputGraph(){
+    ifstream file("input.txt");  
+
+    if (!file){
+        cout<<"Error opening file\n";
+    }
+
+    vector<pair<int,int>> edges; // Stores all edges like (1,2),(3,1)
+    set<int> nodes;  // stores unique nodes only
+
+    int a, b;
+
+    // Step 1: read edges + collect unique nodes
+    while (file >> a >> b) {
+        edges.push_back({a, b});
+        nodes.insert(a);
+        nodes.insert(b);
+    }
+
+    file.close();
+
+    // Step 2: map each node → index
+    map<int, int> nodeIndex;
+    int index = 0;
+
+    for (int node : nodes) {
+        nodeIndex[node] = index++;
+    }
+
+    // Step 3: create graph with correct size
+    Graph g(nodes.size());
+
+    // Step 4: add edges using mapped indices
+    for (auto e : edges) {
+        int from = nodeIndex[e.first];
+        int to   = nodeIndex[e.second];
+
+        g.addEdge(from, to);
+    }
+
+    return g;
+};
 
 // Task 4 - Algorithm to check if a graph is acyclic, with checks to show how answer obtained.
 
@@ -38,7 +88,6 @@ class Graph {
 
 // Program Start
 int main(){
-    cout<<endl;
-    Graph g(3);
+    Graph g = InputGraph();
     g.print();
-}
+};
